bd25f10db15c5b59657ff75e2ae3417d4133a4a4
unknown <23sohamsh@gmail.com>
2021-12-11 19:30:44 +0530

    updated unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+
+const Title = ({text}) => {
+  return (
+    <h2>{text}</h2>
+  )
+}
+
+const Stastistics = ({good, neutral, bad}) => {
+  const all = good + bad + neutral; 
+  const average = good * 1 - bad * 1;
+  if(good === 0 && bad===0 && neutral===0) {
+    return (
+      <>
+      <Title text='statistics'/>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  return (
+    <>
+    <Title text='statistics'/>
+    <table> 
+    <StatisticLine label= 'good'review ={good} />
+    <StatisticLine label= 'neutral'review ={neutral} />
+    <StatisticLine label= 'bad'review ={bad} />
+    <StatisticLine label= 'all'review ={all} />
+    <StatisticLine label= 'average'review ={(average/all).toFixed(2)} />
+    <StatisticLine label= 'postive'review ={`${(good*100/all).toFixed(2)} %`} />
+    </table>
+    </>
+  )
+  
+}
+
+const Button = ({feedback, reviewChange}) => {
+  return (
+    <button onClick= {reviewChange}> {feedback} </button>
+  )
+}
+
+const StatisticLine = ({label, review}) => {
+  return (
+    <tr> 
+    <td> {label} </td> 
+    <td> {review} </td>
+    </tr>
+  )
+}
 
 function App() {
+   // save clicks of each button to its own state
+   const [good, setGood] = useState(0)
+   const [neutral, setNeutral] = useState(0)
+   const [bad, setBad] = useState(0)
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title text='give feedback'/>
+      <Button feedback='good' reviewChange= {()=> setGood(good + 1)}/>
+      <Button feedback='neutral'  reviewChange= {()=> setNeutral(neutral + 1)}/>
+      <Button feedback='bad'  reviewChange= {()=> setBad(bad + 1)}/>
+      <Stastistics good={good} bad={bad} neutral={neutral} />
+
     </div>
   );
 }
