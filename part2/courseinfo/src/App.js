12d1fbae9af2fc4f061838f1874ed244b00e5ce8
unknown <23sohamsh@gmail.com>
2021-12-15 19:48:19 +0530

    part 2.1 updated

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -24,9 +24,7 @@ const Part = (props) => {
 const Content = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      {course.parts.map(part => <Part part= {part} key={part.id}></Part>)}
     </div>
   )
 }
