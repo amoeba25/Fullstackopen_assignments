e055d78b4528b60635632c69c726f94d5e8a9cf8
unknown <23sohamsh@gmail.com>
2021-12-15 20:30:00 +0530

    part 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,44 +1,5 @@
 import React from 'react';
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Total = ({ course }) => {
-
-  const sum = course.parts.reduce((prev, current) => prev + current.exercises, 0); 
-  return(
-    <p><b>Total of {sum} exercises </b></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part part= {part} key={part.id}></Part>)}
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
@@ -90,7 +51,7 @@ const App = () => {
   return (
     <>
     <h1>Web Dev curriculum</h1>
-    {courses.map(course => <Course course={course}></Course>)}
+    {courses.map(course => <Course course={course} key= {course.id}></Course>)}
     </>
     // <Course course= {courses} />
   )
---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+    return (
+      <h2>{course.name}</h2>
+    )
+  }
+  
+  const Total = ({ course }) => {
+  
+    const sum = course.parts.reduce((prev, current) => prev + current.exercises, 0); 
+    return(
+      <p><b>Total of {sum} exercises </b></p>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ course }) => {
+    return (
+      <div>
+        {course.parts.map(part => <Part part= {part} key={part.id}></Part>)}
+      </div>
+    )
+  }
+  
+  const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+export default Course
